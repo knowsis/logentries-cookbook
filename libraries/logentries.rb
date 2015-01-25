9ac61c671e0702db708b894c4cb3cb01f1b7a453
Mark Unsworth <mark@knows.is>
2015-01-25 20:58:45 +0000

    added logging. changed how we pass host to the get_log_key method, now takes host name rather than the host key

--- libraries/logentries.rb
+++ libraries/logentries.rb
@@ -8,6 +8,7 @@ module Knowsis
         module LogEntries
            
             def get_hosts()
+                Chef::Log.debug("Fetching logs for hosts")
                 uri = "https://pull.logentries.com/#{ node[:knowsis][:log_entries][:user_key] }/hosts/"
                 response = JSON.parse(open(uri).read)
                 hosts = response["list"].select {|item| item["object"] == "host"}
@@ -23,7 +24,7 @@ module Knowsis
                 }
                 
                 uri = URI.parse("https://api.logentries.com/")
-                    
+                
                 https = Net::HTTP.new(uri.host,uri.port)
                 https.use_ssl = true
                 
@@ -37,6 +38,8 @@ module Knowsis
             end
 
             def get_logs(host_key)
+
+                Chef::Log.debug("Fetching logs for host: #{host_key}")
                 uri = "https://pull.logentries.com/#{ node[:knowsis][:log_entries][:user_key] }/hosts/#{host_key}/"
                 
                 response = JSON.parse(open(uri).read)
@@ -44,11 +47,16 @@ module Knowsis
                 hosts
             end 
 
-            def get_log_key(logname, host_key)
+            def get_log_key(logname, host_name)
+
+                Chef::Log.debug("Getting log key for log: #{logname} under host #{ host_name}")
+
+                host_key = get_host_key(host_name)
                 
                 log = get_logs(host_key).find { |log| log["name"] == logname}
                 
                 if log.nil? || log.empty?
+                    Chef::Log.debug("Log with name: #{logname} does not exist. Creating new log under host: #{ host_key}")
                     log = create_log(logname, host_key)
                     lof = log["log"]
                 end
@@ -58,7 +66,7 @@ module Knowsis
             end
 
 
-            def create_log(logname, host_key)
+            def create_log(logname, host_key)                
                 params = {
                     "request" => "new_log",
                     "user_key" => node[:knowsis][:log_entries][:user_key],
@@ -84,9 +92,10 @@ module Knowsis
 
 
             def get_host_key(hostname)
-                
+                Chef::Log.debug("Finding host with name: #{ hostname} ")
                 host = get_hosts.find { |host| host["name"] == hostname}
                 if host.nil? || host.empty?
+                    Chef::Log.debug("Host with name: #{hostname} does not exist. Creating new host")
                     host = create_host(hostname)
                     host = host["host"]
                 end
