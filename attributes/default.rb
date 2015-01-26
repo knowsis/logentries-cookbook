90e3736c4728efec9050ad78965bf447b41b5713
Mark Unsworth <mark@knows.is>
2015-01-26 10:47:19 +0000

    Fixed missing config values

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,7 @@ default[:logentries][:user_key] = ""
 default[:logentries][:host_name] = "myhost"
 default[:logentries][:log_name] = "mylog"
 
-node[:logentries][:enable_tls]
-
+# The number of times to retry the sending of failed messages (defaults to unlimited)
+default[:logentries][:resume_retry_count] = -1
 
 default[:logentries][:ssl_cert_location] = "/opt/ssl/logentries.all.crt"
